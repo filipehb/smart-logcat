6bbf0bff7322d0f7e41363bd35d0858a3b53ce1d
Filipe Henrique Benjamim de Arruda <filipe.arruda@inatel.br>
2019-06-03 17:27:30 -0300

    - Adicionado def para tirar screen - corrigido problema de retornar seriais

--- adb_utils.py
+++ adb_utils.py
@@ -1,24 +1,30 @@
+import subprocess
+import datetime
 from adb.client import Client as AdbClient
 
+
 class AdbUtils:
-    def __init__(self, ip = "127.0.0.1", port = 5037):
+    def __init__(self, ip="127.0.0.1", port=5037):
+        subprocess.run(["adb", "devices"])
         self.client = AdbClient(host=ip, port=port)
+        self.devices = []
 
     def get_client_version(self):
         return self.client.version()
 
     def list_all_devices(self):
         for device in self.client.devices():
-            return device.serial
+            self.devices.append(device.serial)
+        return self.devices
 
-    #Exemple "example.package"
+    # Exemple "example.package"
     def is_installed_apk(self, device, apk_package):
         return device.is_installed(apk_package)
 
     def install_apk(self, device, apk_path):
         device.install(apk_path)
 
-    #Exemple "example.package"
+    # Exemple "example.package"
     def uninstall_apk(self, device, apk_package):
         device.install(apk_package)
         return self.is_installed_apk(device, apk_package)
@@ -29,8 +35,26 @@ class AdbUtils:
         full_name = manufacturer + " " + model
         return full_name.replace("\n", "").replace("\r", "")
 
+    def _dump_logcat(connection):
+        while True:
+            data = connection.read(1024)
+            if not data:
+                break
+            print(data.decode('utf-8'))
+        connection.close()
+
+    def get_device_logcat(self, device):
+        return device.shell("logcat", handler=self._dump_logcat)
+
+    def get_screen_shot(self, device):
+        result = device.screencap()
+        with open(datetime.datetime.now().replace(" ","").replace("-","").replace(":","").replace(".","") + ".png", "wb") as fp:
+            fp.write(result)
+
 
 if __name__ == '__main__':
     adb1 = AdbUtils()
-    #print(adb1.list_all_devices())
-    print(adb1.get_device_name(adb1.client.device(serial=adb1.list_all_devices())))
+    # print(adb1.list_all_devices())
+    for device in adb1.list_all_devices():
+        print(adb1.get_device_name(adb1.client.device(serial=device)))
+        print(adb1.get_device_logcat(adb1.client.device(serial=device)))
